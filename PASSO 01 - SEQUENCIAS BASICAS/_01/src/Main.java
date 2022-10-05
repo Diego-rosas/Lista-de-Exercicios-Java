c440fc0cff1d5aa8da8db8c96f34bb9968686f9c
Diego Roberto Gomes Rosas <diego.rosas@ifood.com.br>
2022-10-04 21:54:57 -0300

    Questao 01

---
+++ PASSO 01 - SEQUENCIAS BASICAS/_01/src/Main.java
@@ -0,0 +1,9 @@
+// Funcao : Mostrar a mensagem "Olá, Mundo!"
+// Autor : Diego Rosas
+// Data : 04/10/2022
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
