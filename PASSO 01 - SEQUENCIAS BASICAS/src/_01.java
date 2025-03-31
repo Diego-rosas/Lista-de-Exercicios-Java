9997420a0b013b359034a83a85d7e310108ea793
Diego Rosas <diegorosas19@gmail.com>
2025-03-31 20:55:27 -0300

    update Exercicio 01

--- PASSO 01 - SEQUENCIAS BASICAS/src/_01.java
+++ PASSO 01 - SEQUENCIAS BASICAS/src/_01.java
@@ -1,3 +1,6 @@
+// Funcao : Mostrar a mensagem "Olá, Mundo!"
+// Autor : Diego Rosas
+// Data : 31/03/2025
 public class _01 {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
